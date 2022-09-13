cc63569ed9f959132d15072fe926ef83c2b700c7
fakeboom <fakeboom@foxmail.com>
2022-09-13 21:55:05 +0800

    flatten contract

--- contracts/LeagueOfThrones.sol
+++ contracts/LeagueOfThrones.sol
@@ -1,6 +1,5 @@
-pragma experimental ABIEncoderV2;
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
